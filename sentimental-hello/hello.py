b8b6a6b36bd3b1f7fe8ea98bd2c88f09cf454b65
Arthur Araujo <arthuraraujogaldino@hotmail.com>
2022-07-06 16:58:27 -0300

    Create hello.py

---
+++ sentimental-hello/hello.py
@@ -0,0 +1,4 @@
+# As it executes the inner function first in the chain
+# print will wait for input's return so
+# It will ask user for their name then print
+print("Hello, " + input("What's your name? "))
